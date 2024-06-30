eee623a8bc33f2ea591b03f52ba2e95c7eca0503
LAPTOP-M\81808 <miki.snoopy.729@gmail.com>
2024-06-30 17:47:31 +0900

    ストップウォッチのモーダルウィンドウの作成、モーダルウィンドウの修正

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-const modal = $("#js-modal");
+const omikujiModal = $("#omikuji-modal");
+const stopwatchModal = $("#stopwatch-modal");
 const overlay = $("#js-overlay");
-const close = $("#js-close");
-const open = $("#js-open");
+const omikujiClose = $("#omikuji-close");
+const stopwatchClose = $("#stopwatch-close");
+const omikujiOpen = $("#omikuji-open");
+const stopwatchOpen = $("#stopwatch-open");
 
-// 画像をクリックした時の処理
-open.on('click', function () { //ボタンをクリックしたら
-    modal.addClass("open"); // modalクラスにopenクラス付与
+// おみくじ画像をクリックした時の処理
+omikujiOpen.on('click', function () { //ボタンをクリックしたら
+    omikujiModal.addClass("open"); // modalクラスにopenクラス付与
     overlay.addClass("open"); // overlayクラスにopenクラス付与
 });
+
+// ストップウォッチ画像をクリックした時の処理
+stopwatchOpen.on('click', function () {
+    stopwatchModal.addClass("open");
+    overlay.addClass("open");
+});
 // ×ボタンをクリックした時の処理
-close.on('click', function () { //×ボタンをクリックしたら
-    modal.removeClass("open"); // overlayクラスからopenクラスを外す
+omikujiClose.on('click', function () { //×ボタンをクリックしたら
+    omikujiModal.removeClass("open"); // overlayクラスからopenクラスを外す
     overlay.removeClass("open"); // overlayクラスからopenクラスを外す
 });
+stopwatchClose.on('click', function () {
+    stopwatchModal.removeClass("open");
+    overlay.removeClass("open");
+});
+
 //  黒い部分をクリックした時の処理
 overlay.on("click", function () {
-    modal.removeClass("open"); // overlayクラスからopenクラスを外す
+    omikujiModal.removeClass("open"); // overlayクラスからopenクラスを外す
+    stopwatchModal.removeClass("open");
     overlay.removeClass("open"); // overlayクラスからopenクラスを外す
 })
