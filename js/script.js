1bb4145a15505d20c78024382eecf1e27c9bd416
LAPTOP-M\81808 <miki.snoopy.729@gmail.com>
2025-01-26 18:43:18 +0900

    wearchアプリ追加

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 const omikujiModal = $("#omikuji-modal");
 const stopwatchModal = $("#stopwatch-modal");
+const wearchModal = $("#wearch-modal");
 const overlay = $("#js-overlay");
 const omikujiClose = $("#omikuji-close");
 const stopwatchClose = $("#stopwatch-close");
+const wearchClose = $("#wearch-close");
 const omikujiOpen = $("#omikuji-open");
 const stopwatchOpen = $("#stopwatch-open");
+const wearchOpen = $("#wearch-open");
 
-// おみくじ画像をクリックした時の処理
+// 画像をクリックした時の処理
 omikujiOpen.on('click', function () { //ボタンをクリックしたら
     omikujiModal.addClass("open"); // modalクラスにopenクラス付与
     overlay.addClass("open"); // overlayクラスにopenクラス付与
 });
-
-// ストップウォッチ画像をクリックした時の処理
 stopwatchOpen.on('click', function () {
     stopwatchModal.addClass("open");
     overlay.addClass("open");
 });
+wearchOpen.on('click', function () {
+    wearchModal.addClass("open");
+    overlay.addClass("open");
+});
+
 // ×ボタンをクリックした時の処理
 omikujiClose.on('click', function () { //×ボタンをクリックしたら
     omikujiModal.removeClass("open"); // overlayクラスからopenクラスを外す
@@ -26,10 +32,15 @@ stopwatchClose.on('click', function () {
     stopwatchModal.removeClass("open");
     overlay.removeClass("open");
 });
+wearchClose.on('click', function () {
+    wearchModal.removeClass("open");
+    overlay.removeClass("open");
+});
 
 //  黒い部分をクリックした時の処理
 overlay.on("click", function () {
     omikujiModal.removeClass("open"); // overlayクラスからopenクラスを外す
     stopwatchModal.removeClass("open");
+    wearchModal.removeClass("open");
     overlay.removeClass("open"); // overlayクラスからopenクラスを外す
 })
